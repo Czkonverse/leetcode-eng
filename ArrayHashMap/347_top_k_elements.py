06b682d924298c9581ef47bf07352f2508474738
Kunverse <czkconverse@gmail.com>
2026-09-19 22:11:15 +0800

    update

--- ArrayHashMap/347_top_k_elements.py
+++ ArrayHashMap/347_top_k_elements.py
@@ -27,7 +27,25 @@ class Solution:
 
         unique = list(record.keys())
 
-        pivot_idx = random.randint(0, len(nums) - 1)
+        pivot_idx = random.randint(0, len(unique) - 1)
+        temp_num = unique[-1]
+        unique[-1] = unique[pivot_idx]
+        unique[pivot_idx] = temp_num
+
+        temp_nums = [unique[pivot_idx]]
+        for i in range(0, len(unique) - 2):
+            if record[unique[i]] < record[unique[pivot_idx]]:
+                temp_nums = [record[unique[i]]] + temp_nums
+            else:
+                temp_nums = temp_nums + [record[unique[i]]]
+
+        if pivot_idx > k-1:
+            pass
+        elif pivot_idx < k-1:
+            pass
+        else:
+            pass
+
 
 if __name__ == "__main__":
     nums = [1,1,1,2,2,3]
